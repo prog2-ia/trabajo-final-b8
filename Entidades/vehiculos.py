9a28d164f2bc84310c26a3a922856b3b6288fbd2
alefue2007 <alefue.2007@gmail.com>
2026-02-27 11:36:38 +0100

    Entidad Vehiculo

---
+++ Entidades/vehiculos.py
@@ -0,0 +1,7 @@
+class Vehiculo:
+    def __init__(self, matricula, marca, tipo):
+
+        self.matricula = matricula
+        self.modelo = modelo
+        self.tipo = tipo  # 'Coche' o 'Moto'
+        self.disponible = True
