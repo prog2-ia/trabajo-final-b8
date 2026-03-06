2439c48b61bf0cdff753d0723b118fa1da9fd070
alefue2007 <alefue.2007@gmail.com>
2026-03-06 11:20:43 +0100

    herencia coche y moto + sugerencias de tipo

--- Entidades/vehiculos.py
+++ Entidades/vehiculos.py
@@ -1,7 +1,34 @@
+# Clase vehiculo
 class Vehiculo:
-    def __init__(self, matricula, modelo, marca, tipo):
+    def __init__(self, matricula: str, modelo: str, marca: str, es_electrico: bool, es_hibrido: bool):
 
-        self.matricula = matricula
-        self.modelo = modelo
-        self.tipo = tipo  # 'Coche' o 'Moto'
-        self.disponible = True
+        self.matricula: str = matricula
+        self.modelo: str = modelo
+        self.marca: str = marca
+        self.disponible: bool = True
+        self.es_electrico: bool = es_electrico
+        self.es_hibrido: bool = es_hibrido
+
+# Clase coche, hereda de vehiculo
+class Coche(Vehiculo):
+    def __init__(self, matricula: str, modelo: str, marca: str, es_electrico: bool, es_hibrido: bool, cv: int):
+
+        super().__init__(matricula, modelo, marca, es_electrico, es_hibrido)
+        self.cv: int = cv
+
+# Clase moto, hereda de vehiculo
+class Moto(Vehiculo):
+    def __init__(self, matricula: str, modelo: str, marca: str, es_electrico: bool, es_hibrido: bool, cilindrada: int):
+
+        super().__init__(matricula, modelo, marca, es_electrico, es_hibrido)
+        self.cilindrada: int = cilindrada
+
+
+
+
+
+
+
+
+
+
