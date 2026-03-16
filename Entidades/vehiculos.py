a43bd2ae2173138b169fde284bae1ee511f3244c
alefue2007 <alefue.2007@gmail.com>
2026-03-16 19:14:49 +0100

    Eliminación type hinting archivo vehiculos.

--- Entidades/vehiculos.py
+++ Entidades/vehiculos.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 # Clase base
 class Vehiculo(ABC):
-    def __init__(self, matricula: str, marca: str, modelo: str):
+    def __init__(self, matricula, marca, modelo):
         # Usamos _ para indicar que son atributos protegidos
         self._matricula = matricula
         self._marca = marca
@@ -19,7 +19,7 @@ class Vehiculo(ABC):
 
 # Clase hija coche hereda de vehiculo
 class Coche(Vehiculo):
-    def __init__(self, matricula: str, marca: str, modelo: str, es_automatico: bool):
+    def __init__(self, matricula, marca, modelo, es_automatico):
         # Aprovechamos el constructor de la madre
         super().__init__(matricula, marca, modelo)
         self.es_automatico = es_automatico
@@ -29,7 +29,7 @@ class Coche(Vehiculo):
 
 # Clase hija moto hereda de vehiculo
 class Moto(Vehiculo):
-    def __init__(self, matricula: str, marca: str, modelo: str, cilindrada: int):
+    def __init__(self, matricula, marca, modelo, cilindrada):
         super().__init__(matricula, marca, modelo)
         self.cilindrada = cilindrada
 
